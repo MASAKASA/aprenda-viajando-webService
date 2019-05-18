6dce24662c4f7d91c0ad223a849c38b5a745ed2f
Marcio Almeida <marcioandredasilvaalmeida@gmail.com>
2019-05-18 17:17:46 -0300

    Configurações do ambiente de desenvolvimento

--- aprenda-viajando-webService/src/main/java/br/com/aprendaViajando/AprendaViajandoWebServiceApplication.java
+++ aprenda-viajando-webService/src/main/java/br/com/aprendaViajando/AprendaViajandoWebServiceApplication.java
@@ -1,9 +1,23 @@
 package br.com.aprendaViajando;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EntityScan(basePackages = "br.com.aprendaViajando.domain.model")
+@ComponentScan(basePackages = "br.com.aprendaViajando.*")
+@EnableJpaRepositories(basePackages = "br.com.aprendaViajando.domain.repository")
+@EnableTransactionManagement
+@EnableWebMvc
+@RestController
+@EnableAutoConfiguration
 public class AprendaViajandoWebServiceApplication {
 
 	public static void main(String[] args) {
