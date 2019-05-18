1cb5204a7ae850702c62596a7be591930a0069bf
Marcio Almeida <marcioandredasilvaalmeida@gmail.com>
2019-05-18 17:09:21 -0300

    Crição do projeto

---
+++ aprenda-viajando-webService/src/main/java/br/com/aprendaViajando/AprendaViajandoWebServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.aprendaViajando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendaViajandoWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendaViajandoWebServiceApplication.class, args);
+	}
+
+}
