3fa43be58953a7a2f53b8b7d3dfa7051fb0506a7
Marcio Almeida <marcioandredasilvaalmeida@gmail.com>
2019-05-30 21:05:35 -0300

    Criação dos repositories parte final

--- aprenda-viajando-webService/src/main/java/br/com/aprendaViajando/domain/repository/util/TelefoneRepository.java
+++ aprenda-viajando-webService/src/main/java/br/com/aprendaViajando/domain/repository/util/TelefoneRepository.java
@@ -10,7 +10,21 @@ import br.com.aprendaViajando.domain.model.util.Telefone;
 
 public interface TelefoneRepository extends JpaRepository<Telefone, Long>{
 
-	List<Telefone> findByPontoTuristicoOrderByTipoTelefoneAcs(PontoTuristico pontoTuristico);
+	/**
+	 * Busca por Telefones do usuario conforme o id de campo usuario_id e ordena por campo tipoTelefone
+	 * 
+	 * @param PontoTuristico pontoTuristico com id para busca no banco de dados
+	 * 
+	 * @return List com PontoTuristico encontrados ou null se não nenhum dado encontrado
+	 */
+	List<Telefone> findByPontoTuristicoIdOrderByTipoTelefoneAsc(PontoTuristico pontoTuristico);
 	
-	List<Telefone> findByUsuarioOrderByTipoTelefoneAsc(Usuario usuario);
+	/**
+	 * Busca por Telefones do usuarios conforme o id de campo usuario_id e ordena por campo tipoTelefone
+	 * 
+	 * @param Usuario usuario com id para busca no banco de dados
+	 * 
+	 * @return List com Usuario encontrados ou null se não nenhum dado encontrado
+	 */
+	List<Telefone> findByUsuarioIdOrderByTipoTelefoneAsc(Usuario usuario);
 }
